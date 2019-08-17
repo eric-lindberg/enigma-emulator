9c3820fcda211e48aa892e23d3efce16b6f4e30c
mindiveri <mindiveri@nvidia.com>
2019-08-16 18:19:15 -0700

    Add new Deque_* files to start new Deque Branch

---
+++ Deque_Rotor.py
@@ -0,0 +1,58 @@
+import numpy as np
+import collections
+
+
+class Rotor():
+    Rotor_I = collections.deque('EKMFLGDQVZNTOWYHXUSPAIBRCJ')
+    Rotor_II = collections.deque('AJDKSIRUXBLHWTMCQGZNPYFVOE')
+    Rotor_III = collections.deque('BDFHJLCPRTXVZNYEIWGAKMUSQO')
+    Rotor_IV = collections.deque('ESOVPZJAYQUIRHXLNFTGKDCMWB')
+    Rotor_V = collections.deque('VZBRGITYUPSDNHLXAWMJQOFECK')
+    Rotor_RFL = collections.deque('YRUHQSLDPXNGOKMIEBFZCWVJAT')
+
+    rotorIds = ['I', 'II', 'III', 'IV', 'V', 'RFL']
+    #   "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    def select_rotor(self, rotor_num):
+        encoding = [ord(ch) for ch in self.wirings[rotor_num]]
+        return np.array(encoding)
+
+    def __init__(self, rotorId, pos = '0', ring = '0'):
+        self.id = rotorId.upper()
+        assert self.id in self.rotorIds, "unrecognized Rotor ID " + self.id
+        rotor_num = self.rotorIds.index(self.id)
+
+        self.wiring = self.select_rotor(rotor_num)
+
+        self.position = int(pos)
+        assert self.position >= 0 and self.position < 26, "Initial position must be between 0 and 25"
+
+        self.ring = int(ring)
+        assert self.ring >= 0 and self.ring < 26, "Ring setting must be between 0 and 25"
+
+    def advance(self):
+        self.position += 1
+        if self.position == 26:
+            self.position = 0
+
+    def forward_encode(self, ch):
+        assert ch.isalpha()
+        ch = ch.upper()
+        chidx = ord(ch) - ord('A')
+        idx = (chidx + self.position) % 26
+        idx = (idx - self.ring) % 26
+        # Adjust idx for rings (need to review ring model)
+        chidx = self.wiring[idx] - ord('A')
+        chidx = (chidx + self.ring) % 26
+        return chr(((chidx - self.position) % 26) + ord('A'))
+
+    def backward_encode(self, ch):
+        assert ch.isalpha()
+        ch = ch.upper()
+        chidx = ord(ch) - ord('A')
+        idx = (chidx + self.position) % 26
+        idx = (idx - self.ring) % 26
+        # Adjust idx for rings (need to review ring model)
+        chidx = np.where(self.wiring == idx + ord('A'))[0]
+        chidx = (chidx + self.ring) % 26
+        return chr(((chidx - self.position) % 26) + ord('A'))
---
+++ Deque_RotorTest.py
@@ -0,0 +1,96 @@
+import unittest
+from Deque_Rotor import Rotor
+
+
+class TestRotor(unittest.TestCase):
+    def initRotorsArray(self, position='0', rings='0'):
+        rotors = []
+        rotors.append(Rotor('I', position, rings))
+        rotors.append(Rotor('II', position, rings))
+        rotors.append(Rotor('III', position, rings))
+        rotors.append(Rotor('IV', position, rings))
+        rotors.append(Rotor('V', position, rings))
+
+        return rotors
+
+    def testWirings(self):
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWZYX"
+        rotors = self.initRotorsArray()
+
+        for rotor in rotors:
+            assert len(rotor.wiring) == 26, \
+                "Rotor does not have correct wiring"
+            for letter in alphabet:
+                assert ord(letter) in rotor.wiring, \
+                    "Letter not wired in rotor"
+
+    # Test forward mapping with position and rings at 0
+    def testForwardMapping(self):
+        rotors = self.initRotorsArray()
+        rotors.append(Rotor('RFL', '0', '0'))  # Test Reflector as well
+
+        letters = "EABEVY"
+
+        rotor_idx = 0
+        for rotor in rotors:
+            assert rotor.forward_encode('A') == letters[rotor_idx], \
+                "Failed to encode letter correctly"
+            rotor_idx += 1
+
+    # Test forward mapping with position change
+    def testForwardAdvance(self):
+        rotors = self.initRotorsArray(position = '1')
+        letters = "JICRY"
+
+        rotor_idx = 0
+        for rotor in rotors:
+            assert rotor.forward_encode('A') == letters[rotor_idx], \
+                "Failed to advance rotor correctly"
+            rotor_idx += 1
+
+    # Test forward mapping with ring setting
+    def testForwardRing(self):
+        rotors = self.initRotorsArray(rings = '1')
+        letters = "KFPCL"
+        rotor_idx = 0
+        for rotor in rotors:
+            assert rotor.forward_encode('A') == letters[rotor_idx], \
+                "Failed to apply ring correctly"
+            rotor_idx += 1
+
+    # Test backward mapping with position and rings at 0
+    def testBackwardMapping(self):
+        rotors = self.initRotorsArray()
+        letters = "UATHQ"
+
+        rotor_idx = 0
+        for rotor in rotors:
+            assert rotor.backward_encode('A') == letters[rotor_idx], \
+                "Failed to encode letter correctly"
+            rotor_idx += 1
+
+    # Test backward mapping with position change
+    def testBackwardAdvance(self):
+        rotors = self.initRotorsArray(position = '1')
+        letters = "VIZYB"
+
+        rotor_idx = 0
+        for rotor in rotors:
+            assert rotor.backward_encode('A') == letters[rotor_idx], \
+                "Failed to advance rotor correctly"
+            rotor_idx += 1
+
+    # Test backward mapping with ring setting
+    def testBackwardRing(self):
+        rotors = self.initRotorsArray(rings = '1')
+        letters = "KTNGC"
+
+        rotor_idx = 0
+        for rotor in rotors:
+            assert rotor.backward_encode('A') == letters[rotor_idx], \
+                "Failed to apply ring correctly"
+            rotor_idx += 1
+
+
+if __name__ == '__main__':
+    unittest.main()
