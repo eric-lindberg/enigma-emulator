9fb5c2f2768fb131d907ea132987de281cc07868
Mark <markind@hotmail.com>
2019-08-26 18:01:57 -0700

    Improves full Deque Mechanism Unit Testing again by using a single nested loop to test all characters in all Rotors via Deque Rotation against a hard-coded known good character List

--- Deque_RotorTest.py
+++ Deque_RotorTest.py
@@ -24,29 +24,27 @@ class TestRotor(unittest.TestCase):
             self.assertEqual(len(rotor.Rotor[rotor_id]), 26)
 
         # Test that every character in the Deque Rotor Object accessed by
-        # Deque rotation is the same vs. this known good List of all the
-        # characters in all the Rotors in order
-        chr_count = 0
-        rotor_count = 0
-        for char in 'E','K','M','F','L','G','D','Q','V','Z','N','T','O', \
-                    'W','Y','H','X','U','S','P','A','I','B','R','C','J', \
-                    'A','J','D','K','S','I','R','U','X','B','L','H','W', \
-                    'T','M','C','Q','G','Z','N','P','Y','F','V','O','E', \
-                    'B','D','F','H','J','L','C','P','R','T','X','V','Z', \
-                    'N','Y','E','I','W','G','A','K','M','U','S','Q','O', \
-                    'E','S','O','V','P','Z','J','A','Y','Q','U','I','R', \
-                    'H','X','L','N','F','T','G','K','D','C','M','W','B', \
-                    'V','Z','B','R','G','I','T','Y','U','P','S','D','N', \
-                    'H','L','X','A','W','M','J','Q','O','F','E','C','K', \
-                    'Y','R','U','H','Q','S','L','D','P','X','N','G','O', \
-                    'K','M','I','E','B','F','Z','C','W','V','J','A','T':
-            self.assertEqual(rotor.Rotor[rotor_count][0], char)
-            print(rotor.Rotor[rotor_count][0], char, chr_count, rotor_count)
-            rotor.Rotor[rotor_count].rotate(-1)
-            chr_count += 1
-            if chr_count == 26:
-                rotor_count += 1
-                chr_count = 0
+        # Deque rotation is the same vs. this known good hardcoded List
+        # of all the characters in all the Rotors in order
+
+        chars = [['E','K','M','F','L','G','D','Q','V','Z','N','T','O',
+                  'W','Y','H','X','U','S','P','A','I','B','R','C','J'],
+                 ['A','J','D','K','S','I','R','U','X','B','L','H','W',
+                  'T','M','C','Q','G','Z','N','P','Y','F','V','O','E'],
+                 ['B','D','F','H','J','L','C','P','R','T','X','V','Z',
+                  'N','Y','E','I','W','G','A','K','M','U','S','Q','O'],
+                 ['E','S','O','V','P','Z','J','A','Y','Q','U','I','R',
+                  'H','X','L','N','F','T','G','K','D','C','M','W','B'],
+                 ['V','Z','B','R','G','I','T','Y','U','P','S','D','N',
+                  'H','L','X','A','W','M','J','Q','O','F','E','C','K'],
+                 ['Y','R','U','H','Q','S','L','D','P','X','N','G','O',
+                  'K','M','I','E','B','F','Z','C','W','V','J','A','T']]
+        for rotor_count in range(0, 5):
+            for chr_count in range(0, 25):
+                self.assertEqual(rotor.Rotor[rotor_count][0],
+                                 chars[rotor_count][chr_count])
+                print(rotor.Rotor[rotor_count][0], chars[rotor_count][chr_count])
+                rotor.Rotor[rotor_count].rotate(-1)
 
     # TODO:
     # Test forward mapping with position and rings at 0
